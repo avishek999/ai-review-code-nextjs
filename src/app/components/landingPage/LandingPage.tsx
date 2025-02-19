5a10f38494f40c3fd9db643848d4bc701283cad1
Avishek Prasad <avishekprasad0999@gmail.com>
2025-02-19 21:24:31 +0530

    pnpm added

--- src/app/components/landingPage/LandingPage.tsx
+++ src/app/components/landingPage/LandingPage.tsx
@@ -4,8 +4,8 @@ import SecondaryButton from "../buttons/SecondaryButton";
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="w-full h-[calc(100%-76.8px)] flex justify-center flex-col items-center">
-      <h1 className="text-5xl font-bold text-center ">
+    <div className="w-full h-[calc(100%-76.8px)] flex justify-center flex-col items-center  ">
+      <h1 className="text-6xl font-bold text-center ">
         AI-Powered Code Reviews for <br /> Modern Development
       </h1>
       <p className="text-[var(--secondary-text-color)] mt-4  text-center">
@@ -13,8 +13,8 @@ const LandingPage: React.FC = () => {
         reviews and intelligent feedback
       </p>
       <div className="flex gap-5 mt-5">
-        <PrimaryButton  title="Try for Free" href="'/signup"/>
-        <SecondaryButton  title="Docmantaion" href="/dodumnation" />
+        <PrimaryButton title="Try for Free" href="'/signup" />
+        <SecondaryButton title="Docmantaion" href="/dodumnation" />
       </div>
     </div>
   );
