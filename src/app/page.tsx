23fbde00da1f036a1cbc488b84ab7827cdb238b3
Avishek Prasad <avishekprasad0999@gmail.com>
2025-05-02 18:04:59 +0530

    make page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,14 @@ import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import LandingPage from "@/components/LandingPage";
 import { useAuthContext } from "@/context/AuthContext";
-import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const { isAuthenticated } = useAuthContext();
-
-  const params = useSearchParams();
-  const key = params.toString();
   return (
     <div className="w-full">
       <Navbar isAuthenticated={isAuthenticated} />
 
-      <LandingPage isAuthenticated={isAuthenticated} key={key} />
+      <LandingPage isAuthenticated={isAuthenticated} />
       <Footer />
     </div>
   );
